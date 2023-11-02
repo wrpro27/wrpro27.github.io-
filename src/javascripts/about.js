d12e38997774b0f532fd199db5371179f9d502ec
wrpro27 <wpro27@gmail.com>
2023-11-02 16:29:39 -0400

    Before Class

--- src/javascripts/about.js
+++ src/javascripts/about.js
@@ -1,1 +1,12 @@
 console.log('Hello from about.js!');
+
+const buttonResponse = document.getElementById('response');
+
+const textAreaContent = document.getElementById('feedback');
+
+const submitButton = document.getElementById('submit');
+submitButton.onclick = function collectFeedback() {
+  buttonResponse.innerHTML = `${'<h3>Thanks for your feedback!</h3>'
+    + '<p>Your input is greatly appreciated. If you would like to stay in contact or have any academic inquiries, you can reach me at: wrpro27@terpmail.umd.edu</p>'
+    + '<p>Your Response: '}${textAreaContent.value}</p>`;
+};
